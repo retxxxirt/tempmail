01335a6e8a299845d1f2e6992d1c5128ca15d00a
retxxxirt <retxxxirt@gmail.com>
2020-03-22 12:22:05 +0800

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='tempmail-client',
+    packages=['tempmail'],
+    version='0.1.0',
+    license='MIT',
+    description='Client for temp-mail.org',
+    author='retxxxirt',
+    author_email='retxxirt@gmail.com',
+    url='https://github.com/retxxxirt/tempmail',
+    keywords=['tempmail', 'temp mail', 'temp-mail', 'temp-mail.org', 'temporary email', 'email'],
+    install_requires=['requests==2.23.0', 'random-username==1.0.2']
+)
